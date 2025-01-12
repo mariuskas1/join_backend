f38c03121af0b4290fac568eb60145fdf13f240e
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-12 14:14:07 +0100

    added user_auth_app

--- join/settings.py
+++ join/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'join_app'
+    'rest_framework.authtoken',
+    'join_app',
+    'user_auth_app'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
