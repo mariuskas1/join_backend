bb8f0d42ce9c69943691a574cc090a742ef2a2e1
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-13 16:59:52 +0100

    changed permission settings

--- join/settings.py
+++ join/settings.py
@@ -148,9 +148,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.permissions.AllowAny',
     ]
 }
