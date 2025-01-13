524c8ded6598d1581128c6a7d80d4fd014932de0
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-13 12:03:20 +0100

    added user_auth_app urls

--- join/urls.py
+++ join/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('join_app.api.urls'))
+    path('api/', include('join_app.api.urls')),
+    path('api/', include('user_auth_app.api.urls')),
+
 ]
--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
-    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('register/', RegistrationView.as_view(), name='registration'),
     path('login/', CustomLoginView.as_view(), name='login')
 ]
