d12a83401ca1e31bd69e742e3a30f5c5700c419a
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-14 09:09:11 +0100

    added permission classes

--- join_app/api/views.py
+++ join_app/api/views.py
@@ -1,23 +1,28 @@
 from .serializers import TaskSerializer, SubtaskSerializer, ContactSerializer, UserSerializer
 from join_app.models import Task, Subtask, Contact, User
 from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
 class SubtaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Subtask.objects.all()
     serializer_class = SubtaskSerializer
 
 
 class ContactViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
